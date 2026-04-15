36451ede86f6c5ce6e0a4cb7c79f6271852c48eb
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-04-15 21:19:26 +0600

    Update corner radius of pie chart slices for improved aesthetics

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -41,7 +41,7 @@ const StatsPage = () => {
                         innerRadius="80%"
                         outerRadius="100%"
                         // Corner radius is the rounded edge of each pie slice
-                        cornerRadius="50%"
+                        cornerRadius="10%"
                         fill="#8884d8"
                         // padding angle is the gap between each pie slice
                         paddingAngle={5}
